0eb0f0070a4d29be752f740c241c4150f4eaafbf
Felipe Sanchez <felipe.sdiseno@gmail.com>
2024-10-04 12:08:04 -0500

    se agrego un div en loadingSession[sub]

---
+++ front/src/app/loadingsession/[sub]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Loading() {
+  return (
+    <div>
+      <div className="text-white">
+        <h1>Cargando...</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
