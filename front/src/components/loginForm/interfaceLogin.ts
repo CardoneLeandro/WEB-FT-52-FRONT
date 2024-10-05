75c9502ef6d07e3ddbeeb2bdfa3ae6645461233b
Felipe Sanchez <felipe.sdiseno@gmail.com>
2024-10-05 13:36:07 -0500

    se crearon las interfaces para validateLoginForm

--- front/src/components/loginForm/interfaceLogin.ts
+++ front/src/components/loginForm/interfaceLogin.ts
@@ -18,3 +18,13 @@
 //     accessToken?: string;
 //     providerAccountId?: string;
 //   }
+
+export interface ILoginUser {
+  email: string;
+  password: string;
+}
+
+export interface ILoginError {
+  email?: string;
+  password?: string;
+}
